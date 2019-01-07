adae8130be40da9835327665fd6703833c29e4b1
九夜 <1066179972@qq.com>
2019-01-08 00:49:20 +0800

    ..

---
+++ src/dhDoc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dhailing
+ * Date: 2019/1/8
+ * Time: 0:42
+ */
+
+namespace DhDoc;
+
+
+class dhDoc
+{
+    public function __construct()
+    {
+
+    }
+}
